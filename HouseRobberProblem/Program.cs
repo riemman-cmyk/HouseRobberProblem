631d06e83b21921ff83b227fac4ef28cdd8a7301
riemman-cmyk <nika.nika.khundadze@gmail.com>
2025-02-26 22:56:51 +0400

    Add project files.

---
+++ HouseRobberProblem/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HouseRobberProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] houses = { 2, 7, 9, 3, 1 };
+            Console.WriteLine(Solve(houses));
+        }
+        static int Solve(int[] houses)
+        {
+            int n = houses.Length;
+            int r1; int r2; int prof;
+            r1 = r2 = prof = 0;
+            for (int i = 0; i < n; i++)
+            {
+                prof = Math.Max(r1, r2 + houses[i]);
+                r1 = r2;
+                r2 = prof;
+            }
+            return r2;
+        }
+    }
+}
